2afe35083500e02eb5e9deac700d3618f6528c74
Miklos Sarosi <miklos.sarosi@gmail.com>
2019-07-28 17:22:20 +0000

    saved trial in drink_2

--- assets/js/script-trial.js
+++ assets/js/script-trial.js
@@ -4,9 +4,8 @@
     var emptyGlass = $("#emptyglass");
 
     glassDiv.click(function() {
-        emptyGlass
-        .addClass("hide-glass")
-        .removeClass("show-glass");
+        const fade = { height: 1, transition: 'height 5s' };
+        emptyGlass.css(fade).delay(5000).slideDown(1000);
     });
 
 })();
