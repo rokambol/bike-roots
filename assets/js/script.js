7faca164eef09e8501a25632b0356475d6d1c048
Miklos Sarosi <miklos.sarosi@gmail.com>
2019-07-28 20:40:26 +0000

    animated glass filling done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 (function() {
 
     var glassDiv = $(".glasses");
-    var emptyGlass = $("#emptyglass");
+    var emptyGlass = $(".emptyglass-container");
     var bottle = $("#bottle");
 
     glassDiv.click(function() {
         const fade = { height: 1 };
-        const unfade = { height: 100, transition: 'height 5s swing 10s' };
-        const turn = { transform: 'rotate(-100deg)' }; 
+                const turn = { transform: 'rotate(-100deg)' }; 
         const slide = { left: 710 };
         const lower = { top: 220 };
         
-        emptyGlass.css(fade)/*.delay(5000).css(unfade)*/;
+        emptyGlass.css(fade);
         bottle.css(turn).css(slide).css(lower);
     });
 
