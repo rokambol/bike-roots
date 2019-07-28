37660af5ab84b696ccedd2d236654e743063721f
Miklos Sarosi <miklos.sarosi@gmail.com>
2019-07-28 22:28:38 +0000

    waiter and drink moved to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,36 @@
 (function() {
 
-    var glassDiv = $(".glasses");
+    var glassDiv = $(".jumbotron-text");
+    var cancel = $(".jumbotron-main");
     var emptyGlass = $(".emptyglass-container");
     var bottle = $("#bottle");
 
+    var fade = { height: 1 };
+    var turn = { transform: 'rotate(-100deg)' };
+    var slide = { left: 710 };
+    var lower = { top: 220 };
+
+
     glassDiv.click(function() {
-        const fade = { height: 1 };
-                const turn = { transform: 'rotate(-100deg)' }; 
-        const slide = { left: 710 };
-        const lower = { top: 220 };
-        
+        var originalHeight = $(emptyGlass).height();
+        fade = { height: 1 };
+        turn = { transform: 'rotate(-100deg)' };
+        slide = { left: 710 };
+        lower = { top: 220 };
+
         emptyGlass.css(fade);
         bottle.css(turn).css(slide).css(lower);
     });
+
+    cancel.click(function() {
+        fade = { height: 74 };
+        turn = { transform: 'rotate(0deg)' };
+        slide = { left: 800 };
+        lower = { top: 200 };
+
+        emptyGlass.css(fade);
+        bottle.css(turn).css(slide).css(lower);
+    });
+
 
 })();
