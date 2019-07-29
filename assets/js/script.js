1134e263036aa4a81dda05f35060bb7eb55858cb
Miklos Sarosi <miklos.sarosi@gmail.com>
2019-07-29 08:14:03 +0000

    button control added together with modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 (function() {
-
     var glassDiv = $(".jumbotron-text");
     var cancel = $(".jumbotron-main");
     var emptyGlass = $(".emptyglass-container");
@@ -10,6 +9,17 @@
     var slide = { left: 710 };
     var lower = { top: 220 };
 
+    var ginBtn = $("#gin-btn");
+    var whiskeyBtn = $("#whiskey-btn");
+    var vodkaBtn = $("#vodka-btn");
+    var cocktailsBtn = $("#cocktails-btn");
+    var oJBtn = $("#oj-btn");
+    var beerBtn = $("#beer-btn");
+    var targetDiv1 = $(".jumbotron-text");
+    var targetDiv2 = $(".modal-body p");
+    var texts = ["gin serious text, here comes the more detailed explanation", "gin funny quote, this is shorter",
+        "whiskey serious text, here comes the more detailed explanation", "gin funny quote, this is shorter"
+    ];
 
     glassDiv.click(function() {
         var originalHeight = $(emptyGlass).height();
@@ -32,5 +42,16 @@
         bottle.css(turn).css(slide).css(lower);
     });
 
+    ginBtn.click(function() {
+        targetDiv1.text(texts[0]);
+        targetDiv2.text(texts[1]);
+    });
 
+    ginBtn.click(function() {
+        targetDiv1.text(texts[2]);
+        targetDiv2.text(texts[3]);
+    });
+    
+    $(".modal-body p").text()
+    
 })();
